466b945e56d8ce3d535f761189b5e50ee7fa7ed3
BrianStritch <brians12ca18@gmail.com>
2022-02-07 00:04:32 +0000

    clean up spacing errors

--- evens.py
+++ evens.py
@@ -1,21 +1,25 @@
 def even_number_of_evens(numbers):
     """        LIST OF TEST IDEAS
-    should raise a TypeError with an error message if a list isn't passed in as an argument.
+    should raise a TypeError with an error message if a list
+        isn't passed in as an argument.
     Error message: "A list was not passed into the function"
-    If numbers is empty return False    
+    If numbers is empty return False
     if numbers of even is odd return False
     if number of evens is zero return False
     if the numbers of evens is even return True
     """
-    #return None  changed to true to make test pass
+    # return None  changed to true to make test pass
     return True
+
+
 '''
 When the test was run this function call was also run.
 Let’s prevent that from happening by making  the following change.
-So what is this doing? To keep it  simple, when Python runs a file directly,  
-it names it __main__ and any code  beneath the if statement will only be run  
+So what is this doing? To keep it  simple, when Python runs a file directly,
+it names it __main__ and any code  beneath the if statement will only be run
 if the name of the file is __main__.
 '''
+
 
 if __name__ == '__main__':
     print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -1,22 +1,24 @@
 """   notes for the file
 to run this file you use python3 test_evens.py
 
-the reason why you use the pass statement is that Python is expecting an indented block after the use of a colon,  
-so when you have no code, using the pass statement is valid and allows your code to run error free.
+the reason why you use the pass statement is that Python is expecting an
+indented block after the use of a colon, so when you have no code, using
+the pass statement is valid and allows your code to run error free.
 
 #2 import the class from the evens.py file for testing purposes
 
 """
 
 import unittest
-#2
+# 2
 from evens import even_number_of_evens
+
 
 class TestEvens(unittest.TestCase):
     pass
     # def test_function_returns_true(self):  initial test
     #     self.assertTrue(even_number_of_evens([]))
 
+
 if __name__ == '__main__':
     unittest.main()
-
